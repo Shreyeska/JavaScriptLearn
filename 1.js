99611a0f8aed0d2cf8f490d60dded919ec650ba9
Shreyeska <silwalshreyeska@gmail.com>
2023-12-23 18:50:54 +0545

    First Question

---
+++ 1.js
@@ -0,0 +1,9 @@
+// Function that takes two numbers as input and returns their sum.
+
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(1,2));// shorter way of calculating.
+
+const result = sum(11,22);
+console.log(result);

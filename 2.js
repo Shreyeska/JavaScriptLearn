779ecc9f4bdb03f6f6f8d3d32a678494dea0d191
Shreyeska <silwalshreyeska@gmail.com>
2023-12-23 20:36:59 +0545

    Question number 2

---
+++ 2.js
@@ -0,0 +1,9 @@
+// 2. Function that takes a string as input and returns its length.
+
+function strlen (str){
+    return str.length;
+}
+
+const input = 'This is JavaScript test';
+const len = strlen(input);
+console.log(len); // output = 23

12764beb8d3cecf0b4052dcd2a8db86c4dba8567
Shreyeska <silwalshreyeska@gmail.com>
2023-12-23 22:05:07 +0545

    Third question

---
+++ 3.js
@@ -0,0 +1,13 @@
+// 3. Function that takes an array of numbers as input and returns the sum of all the numbers in the array.
+
+function arrsum(numbers){
+    let sum=0;
+    for (let i=0; i<numbers.length; i++){
+        sum += numbers[i];
+    }
+    return sum;
+}
+const sumArray = [10,20,30,40,50];
+const add = arrsum(sumArray);
+console.log (add);
+

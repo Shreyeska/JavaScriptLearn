0d6e878b0f9117b75926b160a8b9a244eab63b06
Shreyeska <silwalshreyeska@gmail.com>
2023-12-25 01:28:42 +0545

    9

---
+++ 9.js
@@ -0,0 +1,8 @@
+//9. Function that takes two strings as input and checks if they are equal. Return true if they are equal, false otherwise.
+
+function StringCompare(str1, str2){
+    return str1==str2;
+}
+
+console.log (StringCompare ("StringSame", "StringSame"));//Output: true
+console.log (StringCompare ("StringDiff", "StringSame"));//Output: false

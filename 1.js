2177c7a780485ccd519030e76e8ceeaf357101b8
Shreyeska <silwalshreyeska@gmail.com>
2023-12-24 22:33:34 +0545

    1

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@
 function sum(a,b){
     return a+b;
 }
-console.log(sum(1,2));// shorter way of calculating.
+console.log(sum(1,2));// shorter way of calculating. output 33
 
 const result = sum(11,22);
-console.log(result);
+console.log(result);//output 33

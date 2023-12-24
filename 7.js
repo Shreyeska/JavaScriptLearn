22b36c9ec567a86c8b277ca88d382af68bf711e7
Shreyeska <silwalshreyeska@gmail.com>
2023-12-25 01:10:58 +0545

    7

--- 7.js
+++ 7.js
@@ -1,1 +1,8 @@
 //7. Function that takes a number as input and checks if it is divisible by both 3 and 5. Return true if it is, false otherwise.
+
+function divisiblity(number){
+    return number%3==0 && number % 5 == 0;
+}
+
+console.log(divisiblity(21));//Output: false
+console.log(divisiblity(15));//Output: true

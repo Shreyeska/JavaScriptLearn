e87487d13ff55fdf58fa86624beb851d1861360e
Shreyeska <silwalshreyeska@gmail.com>
2023-12-25 16:44:18 +0545

    21

--- 21.js
+++ 21.js
@@ -3,10 +3,11 @@
 function countChar(str, char){
     let count = 0;
     for(let i=0; i<=str.length; i++){
-        if (str[i].includes(char) ){
+        if (str[i]===char ){
             count++;
         }
     }
     return count;
 }
-console.log(countChar("Christmas", 's'));
+console.log(countChar("Joey doesnt share food", 'e'));//Output: 3
+console.log(countChar("Bleh de la Bleh de Blu Blah Bleh", 'e'));// Output: 5

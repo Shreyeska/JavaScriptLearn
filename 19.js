3e197cedee5a04d33b9e369ab5975cc9b44b81fa
Shreyeska <silwalshreyeska@gmail.com>
2023-12-25 16:23:01 +0545

    19

---
+++ 19.js
@@ -0,0 +1,20 @@
+//19. Function that takes a number as input and checks if it's a prime number using a loop. Return true if it's prime, false otherwise.
+
+function isPrime(number){
+    if (number <=1){
+    return false;
+    }
+    
+    for (let i = 2; i < number;i++){
+        if ( number % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime(7));//Output: true;
+console.log(isPrime(18));//Output: false
+
+
+

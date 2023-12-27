89c36b623635955683c0cfc3c40b455d78c51ee5
Shreyeska <silwalshreyeska@gmail.com>
2023-12-28 01:03:43 +0545

    33

---
+++ 33.js
@@ -0,0 +1,12 @@
+//33. Function that takes an array of numbers as input and returns a new array with only unique values (no duplicates).
+function removeDuplicates(numbersArray) {
+    const uniqueNumbersSet = new Set(numbersArray);
+
+    const uniqueNumbersArray = Array.from(uniqueNumbersSet);
+
+    return uniqueNumbersArray;
+}
+
+const numbersArray = [10, 10, 10, 20, 30, 30, 40, 50, 60, 60];
+const result = removeDuplicates(numbersArray);
+console.log(result); // Output: [ 10, 20, 30, 40, 50, 60 ]

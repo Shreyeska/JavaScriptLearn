b7b5ce92748e3883d39240f632bcf14358ea0c37
Shreyeska <silwalshreyeska@gmail.com>
2023-12-29 07:53:47 +0545

    34

---
+++ 34.js
@@ -0,0 +1,11 @@
+// Create an object representing a person with properties: name, age, and city
+
+const person = {
+    name: "Nivia",
+    age: 20,
+    city: "Dallas"
+};
+
+console.log(person.name); // Output: Nivia
+console.log(person.age); // Output: 20
+console.log(person.city); // Output: Dallas

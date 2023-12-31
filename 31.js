4dc71135a6c12cf089c3d566dd5a59f7be1642d1
Shreyeska <silwalshreyeska@gmail.com>
2023-12-31 16:48:55 +0545

    31

--- 31.js
+++ 31.js
@@ -1,16 +1,14 @@
 //31. Function that takes an array of strings as input and returns a new array with the strings reversed.
-function reverseStrings(stringsArray) {
-    const reverseArray = [];
-
-    for(let i = 0; i < stringsArray.length; i++) {
-        const originalString = stringsArray[i];
+function reverseStrings(stringsArray){
+    const reverseArray =[];
+    for (let i=0; i < stringsArray.length; i++){
+        const originalString= stringsArray[i];
         const reverseString = originalString.split('').reverse('').join('');
         reverseArray.push(reverseString);
     }
-
     return reverseArray;
 }
 
-const stringsArray = ['hello', 'world', 'alicin'];
+const stringsArray =['Alicin', 'Rogue', 'Azina', 'Covey'];
 const result = reverseStrings(stringsArray);
-console.log(result); // Output: [ 'olleh', 'dlrow', 'nicila' ]
+console.log (result); // Output: [ 'nicilA', 'eugoR', 'anizA', 'yevoC' ]

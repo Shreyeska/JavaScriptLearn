b186a8a3b96c3179a595e12451e734097722ab01
Shreyeska <silwalshreyeska@gmail.com>
2023-12-31 16:58:24 +0545

    32

--- 32.js
+++ 32.js
@@ -1,10 +1,10 @@
 // Function that takes two arrays as input and returns a new array with the elements of both arrays combined
 
-function combineArray(array1, array2) {
+function combinearray (array1, array2){
     return array1.concat(array2);
 }
 
-const array1 = [10, 20, 30];
-const array2 = [40, 50, 60];
-const combinedArray = combineArray(array1, array2);
-console.log(combinedArray); // Output: [ 10, 20, 30, 40, 50, 60 ]
+const array1 = [10, 20, 30, 40, 50];
+const array2 = [60, 70, 80, 90, 100];
+const result = combinearray(array1, array2);
+console.log(result);

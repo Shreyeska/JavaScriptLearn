740b09959900a520c7ea8f6ffce137beeffda9a3
Shreyeska <silwalshreyeska@gmail.com>
2024-01-02 04:22:55 +0545

    40

---
+++ 40.js
@@ -0,0 +1,9 @@
+//40. Function that takes a car object as input and returns a string with the make, model, and year combined (e.g., "Toyota Corolla 2022").
+
+const car= {
+    make: 'Toyota',
+    model: 'Corolla',
+    year: 2022
+};
+
+console.log (`${car.make} ${car.model} ${car.year}`);

023cc3f601440c2c89424f3885a2ad2890f24d66
Shreyeska <silwalshreyeska@gmail.com>
2024-01-02 04:20:08 +0545

    39

---
+++ 39.js
@@ -0,0 +1,11 @@
+//39. Create an object representing a car with properties: make, model, and year.
+
+const car = {
+    make:'BMW',
+    model: 'i5 Sedan',
+    year: 2024
+};
+
+console.log(`Name: ${car.make}`);
+console.log(`Model: ${car.model}`);
+console.log(`Year: ${car.year}`);

cd53ee2d05d353a0d97d5340153c1c314f994879
Shreyeska <silwalshreyeska@gmail.com>
2024-01-04 00:57:24 +0545

    45

---
+++ 45.js
@@ -0,0 +1,8 @@
+//45. Function that calculates the area of a rectangle. The function should take the width and height as parameters and return the area.
+
+function areaofRectangle(length, breadth){
+    const area = 2 * (length + breadth);
+    return (area);
+}
+
+console.log (`Area of Rectangle is ${areaofRectangle (5, 4)}`);

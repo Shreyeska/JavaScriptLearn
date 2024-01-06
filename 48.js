242d54bf59c6c18d23fa26b7aa54d3a9e5b97c8b
Shreyeska <silwalshreyeska@gmail.com>
2024-01-06 11:23:42 +0545

    48

--- 48.js
+++ 48.js
@@ -1,1 +1,13 @@
 //48. Function that checks if a given word is a palindrome (reads the same forwards and backwards).
+function palindromCheck(string){
+    const reverseString = string.split('').reverse().join('');
+    if (reverseString == string){
+        return ('The string is palindrome');
+    }
+    else{
+        return ('The string is not palindrome');
+    }
+}
+
+console.log(palindromCheck('racecar'));
+console.log(palindromCheck('palindrome'));

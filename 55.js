554defb2af610610daa348a4dedfff27287eb5f5
Shreyeska <silwalshreyeska@gmail.com>
2024-01-08 01:40:57 +0545

    55

---
+++ 55.js
@@ -0,0 +1,9 @@
+//55. Function that reverses an array.
+
+function reverseArray (array){
+    let  reverse = array.reverse();
+    return reverse;
+}
+
+let array = [1,2,3,4,5,6,7,8];
+console.log (reverseArray(array));

0880425ab6f4997c1cccf582bd91e2150edff468
Shreyeska <silwalshreyeska@gmail.com>
2024-01-08 00:23:58 +0545

    51

---
+++ 51.js
@@ -0,0 +1,10 @@
+//51. Function that finds the maximum of two numbers.
+function maxNumber(num1, num2){
+    if (num1 > num2){
+        return num1;
+    }
+    else 
+    return num2;
+}
+
+console.log (`The maximum number is ${maxNumber(5,4)}`);

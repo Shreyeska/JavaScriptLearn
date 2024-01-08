8a92f6b9009413901d04cf1674951ed07dac2a53
Shreyeska <silwalshreyeska@gmail.com>
2024-01-09 00:28:44 +0545

    57

---
+++ 57.js
@@ -0,0 +1,8 @@
+//57. Function that concatenates two arrays.
+function concatArray (array1, array2){
+    return array1.concat(array2);
+}
+
+const array1 = [1,2,3,4,5];
+const array2 = [6,7,8,9];
+console.log(concatArray(array1,array2));

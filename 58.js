547a600941ab92790fc2539177c09d1cfaa4ce3a
Shreyeska <silwalshreyeska@gmail.com>
2024-01-11 00:08:47 +0545

    58

--- 58.js
+++ 58.js
@@ -1,4 +1,15 @@
 //58. Function that generates the nth number in the Fibonacci sequence.
 
+function Fibonacci(number) {
+    let a=1, b=1, c=0, temp= 0;
+    console.log (a , b);
+    for (let i=2; i<number; i++){
+        c = a+b;
+        temp = a;
+        a=b;
+        b=c;
+        console.log (c);
+    }
+}
 
-
+console.log (Fibonacci(6));

1431587eb6259531564ef3db77bf5f07329d29ca
Shreyeska <silwalshreyeska@gmail.com>
2024-01-15 22:28:50 +0545

    29

--- 29.js
+++ 29.js
@@ -1,4 +1,5 @@
 //29. Function that takes an array of strings as input and returns a new array with the strings sorted in alphabetical order.
+
 function AlphaSorting(str1){
     const sorted = str1.slice().sort();
     return sorted;
